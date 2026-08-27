246af798e81943bd2985f7071c937a9593ec2472
ZCZHON <chonthicha2257@gmail.com>
2026-08-27 16:28:14 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,19 +1,16 @@
 <?php
-date_default_timezone_set('Asia/Bangkok');
+$host     = "db";
+$port     = "5432";
+$dbname   = "elephant_db";
+$user     = "postgres";
+$password = "Namzom"; // หรือรหัสผ่านที่คุณตั้งไว้
 
-// ข้อมูลเชื่อมต่อ DB...
-$host = getenv('DB_HOST') ?: 'localhost';
-$port = getenv('DB_PORT') ?: '5432';
-$dbname = getenv('DB_NAME') ?: 'elephant_db_2uc0';
-$user = getenv('DB_USER') ?: 'postgres_elephant';
-$password = getenv('DB_PASSWORD') ?: '2Ea4suoOfjTUjoyFfamIrUqRpSKOIEUt';
+// เติม sslmode=disable ไว้ท้ายสุด
+$conn_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} sslmode=disable";
 
-// เพิ่ม sslmode=require เพื่อให้รองรับ Render PostgreSQL
-$conn_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} sslmode=require";
-
-$db = @pg_connect($conn_string);
+$db = pg_connect($conn_string);
 
 if (!$db) {
-    die("Database Connection Failed: " . error_get_last()['message']);
+    die("Database Connection Failed: " . pg_last_error());
 }
 ?>
